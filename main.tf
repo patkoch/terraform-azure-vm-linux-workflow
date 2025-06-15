bd37870d544292afbb64ea0c4e98377056f1e577
Patrick Koch <patkoch87@gmail.com>
2025-06-15 12:33:34 +0200

    Siumulate failure

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
+# resource "azurerm_subnet" "subnet" {
+#   name                 = var.subnet_name
+#   resource_group_name  = azurerm_resource_group.rg.name
+#   virtual_network_name = azurerm_virtual_network.vnet.name
+#   address_prefixes     = ["10.0.2.0/24"]
+# }
 
 resource "azurerm_public_ip" "pip" {
   name                = var.public_ip_name
