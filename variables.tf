87843b5b1b25e1ab4c0ae0d7dac94abc9ca9d957
Patrick Koch <patkoch87@gmail.com>
2026-02-07 18:01:31 +0100

    Simulate validation error

--- variables.tf
+++ variables.tf
@@ -3,10 +3,10 @@ variable "subscription_id" {
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group where the Azure resources will be created."
-  type        = string
-}
+# variable "resource_group_name" {
+#   description = "The name of the resource group where the Azure resources will be created."
+#   type        = string
+# }
 
 variable "resource_group_location" {
   description = "The Azure region where the resource group will be created."
