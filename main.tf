8509bcff2e97981b791639c1ead1e16af9d6982e
Patrick Koch <patkoch87@gmail.com>
2026-02-07 17:50:14 +0100

    Fix and simulate fmt error

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_virtual_network" "vnet" {
   name                = var.vnet_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  address_space       = ["10.0.0.0/16"]
+  address_space           = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
