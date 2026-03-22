2c0d6fda199b15dcb9fed767696050c66e1457db
Patrick Koch <patkoch87@gmail.com>
2026-03-22 20:27:30 +0100

    Simulate validation error

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  address_space       = ["10.0.0.0/16"]
-}
+# resource "azurerm_virtual_network" "vnet" {
+#   name                = var.vnet_name
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   address_space       = ["10.0.0.0/16"]
+# }
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
